658040f1832c7325e8607bafb331b1a635e78718
yura178500 <yura178500@yandex.ru>
2022-10-15 19:56:21 +0300

    Задача 6 ( 1-3) Исправил

--- src/Main.java
+++ src/Main.java
@@ -2,31 +2,31 @@ public class Main {
        public static void main(String[] args) {
     //Задача 1
     int i = 0;
-    int solary = 0;
+    int solary = 15000;
     int total = 0;
     while (total < 2_459_000) {
         i = i + 1;
-        solary = 15000;
         total = total + solary;
         System.out.println("Месяц"  + i + " сумма накоплений равна" + total);}
 
     // Задача 2
     int i = 1;
-    while (i <= 10) {
-        System.out.printf("используя цикл while" + i);
+    while (; i <= 10; i++) {
+        System.out.printf("используя цикл while" + i);}
 }
     for (int i = 1; i <= 10; i++) {
         System.out.print(" используя оператор for" + i);
     }
     // Задача 3
-    int population = 12000000;
-    for (int i = 1; i <= 10; i++) {
+
     int born = 17;
     int death = 8;
-        PerYear = population / 1000;
-        BirthRate = born * PerYear;
-        DeathInYear = death * PerYear;
-        total = BirthRate - DeathInYear;
+    int population = 12000000;
+    for (int i = 1; i <= 10; i++) {
+    int perYear = population / 1000;
+    int birthRate = born * perYear;
+    int deathInYear = death * perYear;
+    int total = birthRate - deathInYear;
         total = total + total;
         System.out.printf("Год " + i + "численность населения составляет" + total);
 }
